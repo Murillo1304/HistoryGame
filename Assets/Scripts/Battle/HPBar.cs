bdc5074916c5a4884b8475248bf034114dc9d3b7
DESKTOP-DU62UDB\jose_ <josecarlos130498@gmail.com>
2023-05-13 09:34:39 -0500

    Cambio de color de vida

--- Assets/Scripts/Battle/HPBar.cs
+++ Assets/Scripts/Battle/HPBar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HPBar : MonoBehaviour
 {
@@ -9,6 +10,7 @@ public class HPBar : MonoBehaviour
     public void SetHP (float hpNormalized)
     {
         health.transform.localScale = new Vector3(hpNormalized, 1f);
+        ChangeColor(hpNormalized);
     }
 
     public IEnumerator SetHPSmooth(float newHp)
@@ -20,9 +22,26 @@ public class HPBar : MonoBehaviour
         {
             curHP -= changeAmt * Time.deltaTime;
             health.transform.localScale = new Vector3(curHP, 1f);
+            ChangeColor(curHP);
             yield return null;
         }
         health.transform.localScale = new Vector3(newHp, 1f);
 
+    }
+
+    void ChangeColor(float hpNormalized)
+    {
+        if (hpNormalized <= 1 && hpNormalized > 0.50)
+        {
+            health.GetComponent<Image>().color = new Color32(93, 200, 103, 255);
+        }
+        else if (hpNormalized <= 0.50 && hpNormalized > 0.20)
+        {
+            health.GetComponent<Image>().color = new Color32(237, 200, 50, 255);
+        }
+        else
+        {
+            health.GetComponent<Image>().color = new Color32(230, 19, 52, 255);
+        }
     }
 }
